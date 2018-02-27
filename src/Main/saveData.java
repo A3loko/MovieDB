633caa86bcf9c2bf92c55dc555ab33bc24574ed9
Amiteshwar Sidhu <amiteshwarsidhu7@gmail.com>
2018-02-28 01:56:16 +0530

    Servlet Linking tried

---
+++ src/Main/saveData.java
@@ -0,0 +1,11 @@
+package Main;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class saveData extends HttpServlet{
+	public void service(HttpServletRequest req,HttpServletResponse res) {
+		
+	}
+}
